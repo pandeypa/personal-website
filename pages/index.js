bf4310ebc860562fc422d48f13432a458c083e36
pandeypa <114224028+pandeypa@users.noreply.github.com>
2022-12-07 01:15:45 +0700

    upload

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import React from 'react'
+import classnames from 'classNames'
 import styles from '../styles/Home.module.css'
+import pic from '../public/IMG_6534.JPG'
+import Image from 'next/image'
 
 export default function Home() {
+  const [start, setStart] = React.useState(false);
+  const act = ['code', 'swim', 'jump', 'draw', 'love', 'pray', 'fish', 'care'];
+  const [i, seti] = React.useState(5)
+  React.useEffect(() => {
+      const timer =
+        i > 0 && setInterval(() => seti(i + 1), 1000);
+      return () => clearInterval(timer);
+    }, [i]);
   return (
     <div className={styles.container}>
       <Head>
@@ -12,60 +23,34 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+        <div className={start&&styles.updiv}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Hello!
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <h1 className={styles.title}>
+          My name is Pascal
+        </h1>
         </div>
-      </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+        <br></br>
+
+        <button className={classnames(styles.hover, start&&styles.hoverAfter)} onClick={() => setStart(true)} start={start}>click me</button>
+        
+        <Image src={pic} width='600' height='400' className={classnames(styles.imgHide, start&&styles.imgAfter)}/>
+
+        <div className={start&&styles.downdiv}>
+        <p className={styles.description}>
+          Currently I'm learning how to
+          <code className={styles.code}>{act[i%act.length]}</code>
+        </p>
+        </div>
+
+        <div>
+
+        </div>
+
+      </main>
     </div>
   )
 }
